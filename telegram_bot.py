5dc83c2d9aeab90d2fcc66448aecb4544f650b23
DmitriyPudlo <teinalandergor@gmail.com>
2022-05-28 15:49:26 +0500

    Добавить категорию

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,7 @@
 from random import choice
-
 import telebot
 
-token = ''
+token = '5359097635:AAGsjmkiJbmLfEtfFclA-prlkn2fH2Px2G8'
 
 bot = telebot.TeleBot(token)
 
@@ -14,8 +13,8 @@ todos = dict()
 
 HELP = '''
 Список доступных команд:
-* print  - напечать все задачи на заданную дату
-* todo - добавить задачу
+* show  - напечать все задачи на заданную дату
+* add - добавить задачу
 * random - добавить на сегодня случайную задачу
 * help - Напечатать help
 '''
@@ -45,20 +44,25 @@ def random(message):
 def add(message):
     _, date, tail = message.text.split(maxsplit=2)
     task = ' '.join([tail])
+    if len(task) < 3:
+        bot.send_message(message.chat.id, 'Ошибка')
+        return None
     add_todo(date, task)
     bot.send_message(message.chat.id, f'Задача {task} добавлена на дату {date}')
 
 
 @bot.message_handler(commands=['show'])
 def print_(message):
-    date = message.text.split()[1].lower()
-    if date in todos:
-        tasks = ''
-        for task in todos[date]:
-            tasks += f'[ ] {task}\n'
-    else:
-        tasks = 'Такой даты нет'
-    bot.send_message(message.chat.id, tasks)
+    dates = [date.lower() for date in message.text.split()[1:]]
+    for date in dates:
+        if date in todos:
+            tasks = f'На {date} запланипровано:'
+            for task in todos[date]:
+                tasks += f'''
+[ ] {task}'''
+        else:
+            tasks = f'На {date} ничего не запланировано.'
+        bot.send_message(message.chat.id, tasks)
 
 
 bot.polling(none_stop=True)
